dc49bf2c6f04c86b214465ef00ddbd89edc0d4e4
Sawandda-Seth <sawandawork@gmail.com>
2026-06-14 18:46:23 +0300

    Commit changes in debiging code in index.html file

---
+++ script.js
@@ -0,0 +1,5 @@
+// ===================== Expense Class 
+class Expense { constructor( id, description,amnt, Filtercat ){
+    this.id = id; 
+    this.description = description;
+     this.amnt = amnt; this.category = category;
